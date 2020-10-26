0a3644ad7aaf898f243077134949ddaa52b2724f
Manohar Dharmala <manhar336@gmail.com>
2020-10-26 01:22:38 -0700

    Initial commit

---
+++ comments_variables_day3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python means where python installed
+#Single line comments: '',#,""
+#Multi line comments:''' ''',""" """
+
+#Creating variables in python
+firstname = 'Guido'
+MiddleName = "Van"
+Lastname = """Rossum"""
+Invention = '''Python'''
+YEAR = 1980
+
+#calling or accessing variables in python
+print(firstname)
+print(MiddleName)
+print(Lastname)
+print(Invention)
+print(YEAR)
+print("First Name:\t",firstname,"Middle Name:\t",MiddleName,"Lastname:\t",Lastname,"""Invention:\t""",Invention,"Year:\t",YEAR)
+print("Firstname:%s"%(firstname),"Year:%i"%(YEAR))
+
